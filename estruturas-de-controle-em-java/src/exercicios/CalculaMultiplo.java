9f5711618b25a0eb8bdd15fa6acfd07c6ffb9703
deboralili <53949599+deboralili@users.noreply.github.com>
2025-10-20 15:44:47 -0300

    Adicionado os enunciados dos exercicios

--- estruturas-de-controle-em-java/src/exercicios/CalculaMultiplo.java
+++ estruturas-de-controle-em-java/src/exercicios/CalculaMultiplo.java
@@ -1,5 +1,13 @@
 package exercicios;
 
+/*
+ * Exercicio 4: Escreva um código onde o usuário informa um número 
+ * inicial, posteriormente irá informar outros N números, a execução 
+ * do código irá continuar até que o número informado dividido pelo 
+ * primeiro número tenha resto diferente de 0 na divisão, números 
+ * menores que o primeiro número devem ser ignorados
+ */
+
 public class CalculaMultiplo {
 
 }
--- estruturas-de-controle-em-java/src/exercicios/CalculaTabuada.java
+++ estruturas-de-controle-em-java/src/exercicios/CalculaTabuada.java
@@ -1,5 +1,10 @@
 package exercicios;
 
+/*
+ * Exercicio 1: Escreva um código onde o usuário entra com um número 
+ * e seja gerada a tabuada de 1 até 10 desse número;
+ */
+
 public class CalculaTabuada {
 
 }
